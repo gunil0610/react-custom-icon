23054d0dda9df32805a5e2deac79a4313b13f0c1
geonilna <geonil@pfct.co.kr>
2024-12-31 19:12:21 +0900

    feat: import icon svgs for test

--- packages/icons/tests/dynamicImports.spec.tsx
+++ packages/icons/tests/dynamicImports.spec.tsx
@@ -1,13 +1,13 @@
-import { describe, it, expect } from 'vitest';
-import { Suspense, lazy } from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { describe, it, expect } from "vitest";
+import { Suspense, lazy } from "react";
+import { render, waitFor } from "@testing-library/react";
 
-import dynamicIconImports from '../src/dynamicIconImports';
-import { LucideProps } from '../src/types';
+import dynamicIconImports from "../src/dynamicIconImports";
+import { LucideProps } from "../src/types";
 
-describe('Using dynamicImports', () => {
-  it('should render icons dynamically by using the dynamicIconImports module', async () => {
-    interface IconProps extends Omit<LucideProps, 'ref'> {
+describe("Using dynamicImports", () => {
+  it("should render icons dynamically by using the dynamicIconImports module", async () => {
+    interface IconProps extends Omit<LucideProps, "ref"> {
       name: keyof typeof dynamicIconImports;
     }
 
@@ -28,10 +28,10 @@ describe('Using dynamicImports', () => {
         size={48}
         stroke="red"
         absoluteStrokeWidth
-      />,
+      />
     );
 
-    await waitFor(() => getByLabelText('smile'));
+    await waitFor(() => getByLabelText("smile"));
 
     expect(container.innerHTML).toMatchSnapshot();
   });
